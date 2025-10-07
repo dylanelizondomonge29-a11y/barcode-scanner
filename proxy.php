d4a285db9e4c7c6598f29e371cef82915554e849
dylanelizondomonge29-a11y <dylan.elizondo.monge29@gmail.com>
2025-10-07 17:14:34 -0600

    Add files via upload

--- proxy.php
+++ proxy.php
@@ -1,22 +1,30 @@
 <?php
+// proxy.php
 
-if (!isset($_GET['codigo'])) {
-    echo json_encode(['error' => true, 'mensaje' => 'No se recibió código']);
+// Verifica que se hayan pasado los parámetros
+if(!isset($_GET['codigo_barras']) || !isset($_GET['api_key'])){
+    http_response_code(400);
+    echo json_encode(['error' => true, 'mensaje' => 'Faltan parámetros']);
     exit;
 }
 
-$apiKey = '123456';
-$codigo = urlencode($_GET['codigo']);
+// Obtiene los parámetros
+$codigo = urlencode($_GET['codigo_barras']);
+$apiKey = urlencode($_GET['api_key']);
+
+// URL de la API original
 $url = "http://62.146.226.238/codigos/codigo_barras.php?api_key=$apiKey&codigo_barras=$codigo";
 
-
+// Llamada a la API con cURL
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-$response = curl_exec($ch);
+curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+$result = curl_exec($ch);
+$httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 curl_close($ch);
 
-
+// Devuelve la respuesta JSON tal cual
 header('Content-Type: application/json');
-echo $response;
-?>
+http_response_code($httpCode);
+echo $result;
